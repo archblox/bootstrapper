3e2d03fcacc7f34b24a59fafe38b4aac6a55fac1
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-07 22:14:03 +1000

    added stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ARCHBLOXBootstrapper
 {
-    partial class Form1
+    partial class ARCHBLOX
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ARCHBLOX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(577, 134);
+            this.Name = "ARCHBLOX";
+            this.Text = "ARCHBLOX";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ARCHBLOXBootstrapper
 {
-    public partial class Form1 : Form
+    public partial class ARCHBLOX : Form
     {
-        public Form1()
+        public ARCHBLOX()
         {
             InitializeComponent();
         }
